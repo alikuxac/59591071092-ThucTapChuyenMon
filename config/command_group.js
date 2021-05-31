a0029a35781d4090f280259e329583d1841404dc
Alikuxac <lexson270400@gmail.com>
2021-05-31 10:55:09 +0700

    feat(config): add command_group

---
+++ config/command_group.js
@@ -0,0 +1,12 @@
+module.exports = [
+	["actions", "Actions"],
+	["admin", "Admin"],
+	["botowner", "BotOwner"],
+	["fun", "Fun"],
+	["info", "Info"],
+	["level", "Level"],
+	["other", "Other"],
+	["moderation", "Moderation"],
+	["music", "Music"],
+	["voice", "Voice"]
+];
