8bc7b6ad9a94ad2acc1f86f6ab8cd8c72b69f7fa
Alikuxac <lexson270400@gmail.com>
2021-06-14 11:28:55 +0700

    chore(route): add & fix routes

--- dashboard/routes/index.js
+++ dashboard/routes/index.js
@@ -1,14 +1,26 @@
 const index_route = require("express").Router();
 
-index_route.get("/discord", function (req, res) {
-    res.redirect("https://discord.gg/8yfv46W");
+index_route.get("/github", (req, res) => { 
+    res.redirect("https://github.com/alikuxac");
 })
-
+index_route.get("/info", (req, res) => { 
+    res.redirect("https://status.alikuxac.me");
+})
+index_route.get("/status", (req, res) => { 
+    res.redirect("https://status.alikuxac.me");
+})
+index_route.get("/discord", function (req, res) {
+    res.redirect(`${process.env.INVITE}`);
+})
 index_route.use('/manage', require('./manage'))
 index_route.use("/authorize", require("./authorize"));
+index_route.get("/invite", (req, res) => {
+    res.redirect(`https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}6&permissions=8&scope=bot+applications.commandsresponse_type=code&redirect_uri=${process.env.CALLBACK_URL}`)
+})
+
 
 index_route.get('/', (req, res, next) => {
-    res.render('index', { pageTitle: 'Home', user: req.session.user || null, servers: req.client.guilds.cache.size , users: req.client.users.cache.size });
+    res.render('index', { pageTitle: 'Home', user: req.session.user || null, servers: req.client.guilds.cache.size, users: req.client.users.cache.size, client: req.client });
 })
 
 module.exports = index_route;
