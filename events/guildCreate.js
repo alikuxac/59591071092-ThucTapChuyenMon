df13bee2ecc7c387b883d7bf061e9aaf7c9d4dff
Alikuxac <lexson270400@gmail.com>
2021-06-14 11:00:57 +0700

    fix(guildCreate): fix update settings if exist

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,15 +5,19 @@ module.exports = async (client, guild) => {
 
     guildsettingskeys.prefix = process.env.PREFIX;
 
-    const guildSettings = client.provider.initGuildMap(guild.id);
-    for (const key in guildsettingskeys) {
-        if (!guildSettings[key] && guildSettings[key] === "undefined") {
-            guildSettings[key] = guildsettingskeys[key];
+    if (client.provider.getGuild(guild.id, "guildID")) {
+        const guildSettings = client.provider.guildSettings.get(guild.id);
+        for (const key in guildsettingskeys) {
+            if (!guildSettings[key] && guildSettings[key] === 'undefined') {
+                guildSettings[key] = guildsettingskeys[key];
+            }
         }
+        await client.provider.setGuildComplete(guild.id, guildSettings);
+    } else {
+        await client.provider.reloadGuild(guild.id);
     }
-    await client.provider.setGuildComplete(guild.id, guildSettings);
 
-    if (process.env.JOIN_LEAVE_LOG){
-    client.channels.cache.get(process.env.JOIN_LEAVE_LOG).send(`✅ Joined guild **${guild.name}**. Total server(s): ${client.guilds.cache.size}.`);
+    if (process.env.JOIN_LEAVE_LOG) {
+        client.channels.cache.get(process.env.JOIN_LEAVE_LOG).send(`✅ Joined guild **${guild.name}**. Total server(s): ${client.guilds.cache.size}.`);
     }
 }
